2dd3123522e71b6e486e2c77c3a851f1f132261b
Nesl mac <neslxzhen@gmail.com>
2021-12-20 19:08:46 +0800

    refac: usePdf

--- src/hooks/usePdf.ts
+++ src/hooks/usePdf.ts
@@ -17,51 +17,14 @@ export const usePdf = () => {
   const [isFirstPage, setIsFirstPage] = useState(false);
   const [isLastPage, setIsLastPage] = useState(false);
   const [isSaving, setIsSaving] = useState(false);
-  const currentPage = pages[pageIndex];
-
   const setDimensionsHandler = useCallback(setDimensions, [setDimensions]);
 
-  const nextPage = () => {
-    const newPageIndex = pageIndex + 1;
-    setPageIndex(pageIndex + 1);
-    setIsFirstPage(newPageIndex === 0);
-    setIsLastPage(newPageIndex === pages.length - 1);
-  };
-
-  const previousPage = () => {
-    const newPageIndex = pageIndex - 1;
-    setPageIndex(newPageIndex);
-    setIsFirstPage(newPageIndex === 0);
-    setIsLastPage(newPageIndex === pages.length - 1);
-  };
-
-  const initialize = ({ name, file, pages: _pages }: Pdf) => {
-    const multi = _pages.length > 1;
-    setName(name);
-    setFile(file);
-    setPages(_pages);
-    setPageIndex(0);
-    setIsMultiPage(multi);
-    setIsFirstPage(true);
-    setIsLastPage(_pages.length === 1);
-  };
-
-  const savePdf = async (attachments: Attachments[]) => {
-    if (isSaving || !file) return;
-
-    setIsSaving(true);
-
-    try {
-      await save(file, attachments, name);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setIsSaving(false);
-    }
-  };
-
   return {
-    currentPage,
+    isMultiPage,
+    isFirstPage,
+    isLastPage,
+    isSaving,
+    currentPage: pages[pageIndex],
     dimensions,
     setDimensions: setDimensionsHandler,
     name,
@@ -70,14 +33,41 @@ export const usePdf = () => {
     setPageIndex,
     file,
     setFile,
-    nextPage,
     pages,
-    savePdf,
-    initialize,
-    isMultiPage,
-    previousPage,
-    isFirstPage,
-    isLastPage,
-    isSaving,
+    nextPage: () => {
+      const newPageIndex = pageIndex + 1;
+      setPageIndex(pageIndex + 1);
+      setIsFirstPage(newPageIndex === 0);
+      setIsLastPage(newPageIndex === pages.length - 1);
+    },
+    savePdf: async (attachments: Attachments[]) => {
+      if (isSaving || !file) return;
+
+      setIsSaving(true);
+
+      try {
+        await save(file, attachments, name);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setIsSaving(false);
+      }
+    },
+    setPdf: ({ name, file, pages: _pages }: Pdf) => {
+      const multi = _pages.length > 1;
+      setName(name);
+      setFile(file);
+      setPages(_pages);
+      setPageIndex(0);
+      setIsMultiPage(multi);
+      setIsFirstPage(true);
+      setIsLastPage(_pages.length === 1);
+    },
+    previousPage: () => {
+      const newPageIndex = pageIndex - 1;
+      setPageIndex(newPageIndex);
+      setIsFirstPage(newPageIndex === 0);
+      setIsLastPage(newPageIndex === pages.length - 1);
+    },
   };
 };

a1be155a349cf6770953a7cf15ac0adf52336560
Nesl mac <neslxzhen@gmail.com>
2022-01-10 15:03:10 +0800

    Updates

---
+++ precache-manifest.7f212961eaf603c92a82e9fca77af37a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e7bde7fae693eeb6e70399a8a24bfe",
+    "url": "/pdf-editor/index.html"
+  },
+  {
+    "revision": "28f77689388edb623df3",
+    "url": "/pdf-editor/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "0779d9a4664a2b62674f",
+    "url": "/pdf-editor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "28f77689388edb623df3",
+    "url": "/pdf-editor/static/js/2.b22d7543.chunk.js"
+  },
+  {
+    "revision": "ae6bb62977b25ec73f0509e3cbccba19",
+    "url": "/pdf-editor/static/js/2.b22d7543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0779d9a4664a2b62674f",
+    "url": "/pdf-editor/static/js/main.a4fc3027.chunk.js"
+  },
+  {
+    "revision": "8ec619afac877d34a6e2",
+    "url": "/pdf-editor/static/js/runtime-main.cdf2ed67.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pdf-editor/precache-manifest.7f212961eaf603c92a82e9fca77af37a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pdf-editor/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
